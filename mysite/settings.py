b433db530f037e854baa62b4064c0739295c8ab5
ddusi <ddusi@kakao.com>
2021-03-07 18:48:52 +0900

    Add apps ( client, logistics, order, payment )

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'client',
+    'apps.client',
+    'apps.logistics',
+    'apps.order',
+    'apps.payment',
 ]
 
 MIDDLEWARE = [

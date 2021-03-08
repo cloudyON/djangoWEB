4653a8c95e7ec6153ae3db8f939db54d934683bf
ddusi <ddusi@kakao.com>
2021-03-08 16:48:24 +0900

    models migrations files - Add timestamp (created, modified) - Create product history Table - Add Activate

--- apps/logistics/models.py
+++ apps/logistics/models.py
@@ -1,1 +1,36 @@
-# Create your models here.
+from django.db import models
+from django_extensions.db.models import TimeStampedModel, ActivatorModel
+from simple_history.models import HistoricalRecords
+
+
+class Product(TimeStampedModel, ActivatorModel):
+    class Size(models.TextChoices):
+        null = ''
+        Tall = 'tall'
+        Grande = 'grande'
+        Venti = 'venti'
+
+    class Kind(models.TextChoices):
+        null = ''
+        Coffee = 'coffee'
+        Tea = 'tea'
+        MD = 'MD_product'
+        Frappuccino = 'frappuccino'
+        Blended = 'blended'
+
+    pd_num = models.CharField('상품번호', max_length=200)
+    name = models.CharField('상품명', max_length=200)
+    price = models.IntegerField('가격', default=0)
+    kind = models.CharField('상품 종류', choices=Kind.choices, max_length=200, default='')
+    size = models.CharField('사이즈', choices=Size.choices, max_length=8, blank=True, default='')
+    history = HistoricalRecords(
+        history_change_reason_field=models.TextField(null=True)
+    )
+
+    class Meta:
+        verbose_name = '상품'
+        verbose_name_plural = '상품 모음'
+        ordering = ['pd_num', ]
+
+    def __str__(self):
+        return f'{self.pd_num} {self.name}'
